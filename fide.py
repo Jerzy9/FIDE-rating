c51f93c8d4dcb20f58f32f5b74b71edd4f468e8b
Jerry9 <zbylutkonrad@gmail.com>
2021-12-19 21:02:31 +0100

    design and error handling

--- fide.py
+++ fide.py
@@ -1,34 +1,27 @@
 import math
 
-# Function to calculate the Probability
 def Probability(rating1, rating2):
     return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
 
 
-# Function to calculate Elo rating
-# K is a constant.
-# score determines who won
-#   1 - you won,
-#   0.5d - draw,
-#   0 - you lost)
+# K jest stała - może byc obliczona ze wzoru
+# 800 / (liczba gier na których opiera się obecny ranking gracza + liczba gier zagranych w obecnym turnieju)
 def EloRating(yours_rating, opponents_rating, K, score):
     your_p = Probability(yours_rating, opponents_rating)
     opponents_p = Probability(opponents_rating, yours_rating)
 
-    # win
+    # wygrana
     if score == 1:
         yours_diff = K * (1 - opponents_p)
         opponents_diff = K * (0 - your_p)
-    # draw
+    # remis
     elif score == 0.5:
         yours_diff = K * (0.5 - opponents_p)
         opponents_diff = K * (0.5 - your_p)
-    # lose
+    # przegrana
     else:
         yours_diff = K * (0 - opponents_p)
         opponents_diff = K * (1 - your_p)
-
-    print(score)
 
     yours_diff = round(yours_diff, 1)
     opponents_diff = round(opponents_diff, 1)
@@ -36,13 +29,5 @@ def EloRating(yours_rating, opponents_rating, K, score):
     yours_new_rating = yours_rating + yours_diff
     opponents_new_rating = opponents_rating + opponents_diff
 
-    print("Yours ELO:", yours_rating, "+", yours_diff, "=", yours_new_rating)
-    print("Opponents ELO:", opponents_rating, "+", opponents_diff, "=", opponents_new_rating)
 
-    return yours_rating, opponents_rating, yours_diff, opponents_diff, yours_new_rating, opponents_new_rating
-
-# Ra = 1200
-# Rb = 1000
-# K = 30
-# d = 0.5
-# EloRating(Ra, Rb, K, d)
+    return yours_new_rating, opponents_new_rating
--- main.py
+++ main.py
@@ -3,38 +3,30 @@ import fide
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    Ra = 1200
-    Rb = 1000
-    K = 30
-    d = 1
-    yours_rating, opponents_rating, yours_diff, opponents_diff, yours_new_rating, opponents_new_rating = fide.EloRating(Ra, Rb, K, d)
 
-    return render_template("index.html",
-                           yours_rating=yours_rating,
-                           opponents_rating=opponents_rating,
-                           yours_diff=yours_diff,
-                           opponents_diff=opponents_diff,
-                           yours_new_rating=yours_new_rating,
-                           opponents_new_rating=opponents_new_rating)
+@app.route("/")
+def start():
+    return render_template("index.html")
 
 
 @app.route("/calculate", methods=["POST"])
 def calculate():
-    your_rating = float(request.form["your_rating"])
-    opponents_rating = float(request.form["opponents_rating"])
-    score = float(request.form["score"])
-    K = float(request.form["K"])
+    your_rating, opponents_rating, K, score = 0, 0, 0, 0
+    try:
+        your_rating = float(request.form["your_rating"])
+        opponents_rating = float(request.form["opponents_rating"])
+        score = float(request.form["score"])
+        K = float(request.form["K"])
 
-    yours_rating, opponents_rating, yours_diff, opponents_diff, yours_new_rating, opponents_new_rating = \
+    except:
+        return render_template("index.html",
+                               yours_new_rating="błąd",
+                               opponents_new_rating="błąd")
+
+    yours_new_rating, opponents_new_rating = \
         fide.EloRating(your_rating, opponents_rating, K, score)
 
     return render_template("index.html",
-                           yours_rating=yours_rating,
-                           opponents_rating=opponents_rating,
-                           yours_diff=yours_diff,
-                           opponents_diff=opponents_diff,
                            yours_new_rating=yours_new_rating,
                            opponents_new_rating=opponents_new_rating)
 
